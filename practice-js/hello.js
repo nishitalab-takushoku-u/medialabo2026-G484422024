f6ce24c5dbae9c5b72b94e8dcdb2a1744ca019c0
G484422024 <258442@st.takushoku-u.ac.jp>
2026-06-18 16:45:48 +0900

    JavaScript課題　第3回

--- practice-js/hello.js
+++ practice-js/hello.js
@@ -1,1 +1,20 @@
-console.log('こんにちは');
+let place = "八王子市館町";
+
+let rooms = ["D101", "D102", "D103", "D201", "D202", "D203", "D204", "D205"];
+
+let departments = [
+  "機械システム工学科",
+  "電子システム工学科",
+  "情報工学科",
+  "デザイン学科"
+];
+
+console.log(place);
+
+for (let room of rooms) {
+  console.log(room);
+}
+
+for (let dept of departments) {
+  console.log(dept);
+}
